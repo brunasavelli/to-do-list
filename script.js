6049937905ae6a645d81e4e4d1bb79217d276ad0
Bruna Savelli <bnsavelli@gmail.com>
2025-02-09 13:28:26 -0300

    feat: started js

--- script.js
+++ script.js
@@ -1,10 +1,93 @@
-var lista = document.createElement('ul');
+const inputElement = document.querySelector(".nova-tarefa-input");
+const addTaskButton = document.querySelector(".nova-tarefa-button");
 
-var tarefas = [];
-tarefas.forEach(function(tarefa) {
-    var list = document.createElement("list");
-    list.innerHTML = tarefa;
-    lista.append(list);
-});
+const tasksContainer = document.querySelector(".tarefas-container");
 
-document.body.append(lista);
+const validateInput = () => inputElement.value.trim().length > 0;
+
+const handleAddTask = () => {
+    const inputIsValid = validateInput();
+
+    console.log(inputIsValid);
+
+    if(!inputElement) {
+        return inputElement.classList.add("error");
+    }
+
+    const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+
+    const taskContent = document.createElement("p");
+    taskContent.innerText = inputElement.value;
+
+    taskContent.addEventListener("click", () => handleClick(taskContent));
+
+    const deleteItem = document.createElement("i");
+    deleteItem.classList.add("far");
+    deleteItem.classList.add("far-trash-alt");
+
+    deleteItem.addEventListener("click", () => 
+        handleDeleteClick(taskItemContainer, taskContent)
+    );
+
+    taskItemContainer.appendChild(taskContent);
+    taskItemContainer.appendChild(deleteItem);
+
+    tasksContainer.appendChild(taskItemContainer);
+
+    inputElement.value = "";
+
+    updateLocalStorage();
+};
+
+// const updateLocalStorage = () => {
+//     const tasks = tasksContainer.childNodes;
+
+//     const localStorageTasks = [...tasks].map((task) => {
+//         const content = task.firstChild;
+//         const isCompleted = content.classList.contains("completed");
+
+//         return { description: content.innerText, isCompleted };
+//     });
+
+//     localStorage.setItem("tasks", JSON.stringify(localStorageTasks));
+// };
+
+//     const refreshTasksUsingLocalStorage = () => {
+//         const tasksFromLocalStorage = JSON.parse(localStorage.getItem("tasks"));
+  
+//     if (!tasksFromLocalStorage) return;
+  
+//     for (const task of tasksFromLocalStorage) {
+//       const taskItemContainer = document.createElement("div");
+//       taskItemContainer.classList.add("task-item");
+  
+//       const taskContent = document.createElement("p");
+//       taskContent.innerText = task.description;
+  
+//       if (task.isCompleted) {
+//         taskContent.classList.add("completed");
+//       }
+  
+//       taskContent.addEventListener("click", () => handleClick(taskContent));
+  
+//       const deleteItem = document.createElement("i");
+//       deleteItem.classList.add("far");
+//       deleteItem.classList.add("fa-trash-alt");
+  
+//       deleteItem.addEventListener("click", () =>
+//         handleDeleteClick(taskItemContainer, taskContent)
+//       );
+  
+//       taskItemContainer.appendChild(taskContent);
+//       taskItemContainer.appendChild(deleteItem);
+  
+//       tasksContainer.appendChild(taskItemContainer);
+//     }
+//   };
+  
+//   refreshTasksUsingLocalStorage();
+  
+//   addTaskButton.addEventListener("click", () => handleAddTask());
+  
+//   inputElement.addEventListener("change", () => handleInputChange());
